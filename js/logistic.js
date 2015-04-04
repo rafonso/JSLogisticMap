3f1c4e9c5b5e327d20acd51e410bdfb2139dff74
Rafael Ubiratam Clemente Afonso <rucafonso@hotmail.com>
2015-04-03 21:33:47 -0300

    style adjusts.

--- js/logistic.js
+++ js/logistic.js
@@ -3,16 +3,51 @@
 var jqPlotChart, flotChart;
 
 function centralize() {
-    function f(chart, plot) {
-        $("#" + chart).position({
-            my : "center center",
-            at : "center center",
-            of : "#" + plot
-        });
+    //f("jqPlotChart", "plotJqPlot");
+    $("body > *").position({
+        of : "body"
+    });
+}
+
+function getData() {
+    function f(x, r) {
+        return (r * x * (1 - x));
     }
 
-    //f("jqPlotChart", "plotJqPlot");
-    f("flotChart", "plots")
+    var data = [[], [[0, 0], [1, 1]], [], []];
+
+    var r = $("#rValue").spinner("value");
+    for (var x = 0; x <= 1.02; x += 0.02) {
+        data[0].push([x, f(x, r)]);
+    }
+
+    var x = $("#x0Value").spinner("value"); ;
+    data[3].push(x);
+    for (var it = 1; it < 100; it++) {
+        x = f(x, r);
+        data[3].push(x);
+    }
+
+    x = data[3][0];
+    var y = 0;
+    data[2].push([x, y]);
+    for (var it = 1; it < data[3].length; it++) {
+        y = data[3][it];
+        data[2].push([x, x]);
+        data[2].push([x, y]);
+        x = y;
+    }
+    data[2].splice(1, 1); // workaround
+    //    console.debug(data[2]);
+
+    return data;
+}
+
+function refreshCharts() {
+    var data = getData();
+
+    flotChart.setData(dataToFlotData(data));
+    flotChart.draw();
 }
 
 function initRValue() {
@@ -35,40 +70,6 @@ function initX0Value() {
         spin : refreshCharts,
         change : refreshCharts
     });
-}
-
-function getData() {
-    function f(x, r) {
-        return (r * x * (1 - x));
-    }
-
-    var data = [[], [[0, 0], [1, 1]], [], []];
-
-    var r = $("#rValue").spinner("value");
-    for (var x = 0; x <= 1.02; x += 0.02) {
-        data[0].push([x, f(x, r)]);
-    }
-
-    var x = $("#x0Value").spinner("value");;
-    data[3].push(x);
-    for (var it = 1; it < 100; it++) {
-        x = f(x, r);
-        data[3].push(x);
-    }
-
-    x =  data[3][0];
-    var y = 0;
-    data[2].push([x, y]);
-    for(var it = 1; it < data[3].length; it++) {
-        y = data[3][it];
-        data[2].push([x, x]);
-        data[2].push([x, y]);
-        x = y;
-    }
-    data[2].splice(1, 1); // workaround 
-//    console.debug(data[2]);
-
-    return data;
 }
 
 function dataToFlotData(data) {
@@ -98,13 +99,6 @@ function plotFlotchart(data) {
         });
 }
 
-function refreshCharts() {
-    var data = getData();
-
-    flotChart.setData(dataToFlotData(data));
-    flotChart.draw();
-}
-
 $(document).ready(function () {
     $(window).resize(centralize);
 
@@ -112,6 +106,6 @@ $(document).ready(function () {
     initRValue();
     initX0Value();
     var initialData = getData();
-//    plotJqPlotChart(initialData);
+    //    plotJqPlotChart(initialData);
     plotFlotchart(initialData);
 });
