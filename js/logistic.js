820a68a960997c7b8175174f790fcde0e99fc15d
Rafael Ubiratam Clemente Afonso <rucafonso@hotmail.com>
2015-04-03 23:07:02 -0300

    Control of the number of iteractions.

--- js/logistic.js
+++ js/logistic.js
@@ -1,9 +1,8 @@
 // 123
 
-var jqPlotChart, flotChart;
+var flotChart, iteractionsChart;
 
 function centralize() {
-    //f("jqPlotChart", "plotJqPlot");
     $("body > *").position({
         of : "body"
     });
@@ -14,31 +13,40 @@ function getData() {
         return (r * x * (1 - x));
     }
 
-    var data = [[], [[0, 0], [1, 1]], [], []];
+    var data = {
+        parabol : [],
+        line : [[0, 0], [1, 1]],
+        logistic : [],
+        iteractions : []
+    };
 
     var r = $("#rValue").spinner("value");
     for (var x = 0; x <= 1.02; x += 0.02) {
-        data[0].push([x, f(x, r)]);
+        data.parabol.push([x, f(x, r)]);
     }
 
-    var x = $("#x0Value").spinner("value"); ;
-    data[3].push(x);
-    for (var it = 1; it < 100; it++) {
+    var x = $("#x0Value").spinner("value"); 
+    var numberOfIeractions = $("#iteractionsValue").spinner("value");;
+    data.iteractions.push(x);
+    for (var it = 0; it < numberOfIeractions; it++) {
         x = f(x, r);
-        data[3].push(x);
+        data.iteractions.push(x);
     }
 
-    x = data[3][0];
+    x = data.iteractions[0];
     var y = 0;
-    data[2].push([x, y]);
-    for (var it = 1; it < data[3].length; it++) {
-        y = data[3][it];
-        data[2].push([x, x]);
-        data[2].push([x, y]);
+    data.logistic.push([x, y]);
+    for (var it = 1; it < data.iteractions.length; it++) {
+        y = data.iteractions[it];
+        data.logistic.push([x, x]);
+        data.logistic.push([x, y]);
         x = y;
     }
-    data[2].splice(1, 1); // workaround
-    //    console.debug(data[2]);
+    data.logistic.splice(1, 1); // workaround
+
+    for (var it = 0; it < data.iteractions.length; it++) {
+        data.iteractions[it] = [it, data.iteractions[it]];
+    }
 
     return data;
 }
@@ -72,6 +80,14 @@ function initX0Value() {
     });
 }
 
+function initIteractionsValue() {
+    $("#iteractionsValue").spinner({
+        max : 2000,
+        spin : refreshCharts,
+        change : refreshCharts
+    });
+}
+
 function dataToFlotData(data) {
     function getData(colorIndex, dataIndex) {
         return {
@@ -79,11 +95,12 @@ function dataToFlotData(data) {
             data : data[dataIndex],
             lines : {
                 show : true,
+                lineWidth: 2,
             }
         };
     }
 
-    return [getData(1, 0), getData(0, 1), getData(2, 2)];
+    return [getData(1, "parabol"), getData(0, "line"), getData(2, "logistic")];
 }
 
 function plotFlotchart(data) {
@@ -97,6 +114,18 @@ function plotFlotchart(data) {
             xaxis : axisConfig,
             yaxis : axisConfig,
         });
+    /*
+    var iteractionsAxisConfig = {
+    min : 0,
+    max : data[3].lenght,
+    //        ticks : 11,
+    //        tickDecimals : 1,
+    };
+    iteractionsChart = $.plot("#iteractionsChart", data[3], {
+    xaxis : iteractionsAxisConfig,
+    yaxis : iteractionsAxisConfig,
+    });
+     */
 }
 
 $(document).ready(function () {
@@ -105,6 +134,7 @@ $(document).ready(function () {
     centralize();
     initRValue();
     initX0Value();
+    initIteractionsValue();
     var initialData = getData();
     //    plotJqPlotChart(initialData);
     plotFlotchart(initialData);
