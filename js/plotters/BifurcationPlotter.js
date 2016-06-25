453a5ebb554c7265e73184b345d30725e512ab88
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2016-06-25 10:44:03 -0300

    Legends Adjusts.

--- js/plotters/BifurcationPlotter.js
+++ js/plotters/BifurcationPlotter.js
@@ -3,7 +3,7 @@
 class BifurcationPlotter extends Plotter {
 
 	constructor() {
-		super('bifurcation', { left: 0.06, top: 0.02, right: 0.98, bottom: 0.96, equalXY: false, maxY: 4.0, yTicks: 0.2 });
+		super('bifurcation', { left: 0.06, top: 0.03, right: 0.98, bottom: 0.96, equalXY: false, maxY: 4.0, yTicks: 0.2 });
 		// invert y-Axis: https://forum.jquery.com/topic/jquery-svg-how-to-flip-object
 		this.plot.xAxis.scale(0.0, 1).ticks(0.1, 0, 0).title("").end()
 
@@ -23,15 +23,16 @@ class BifurcationPlotter extends Plotter {
 
 		var _chart = this.chart;
 		var _plot = this.plot;
-		this.plot.bind("click", function(event) {
+
+		this.plot.bind("mousemove", function (event) {
 			var x = _plot.chartToX(event.offsetX);
 			var y = _plot.chartToY(event.offsetY);
-			console.log(event.offsetX, event.offsetY);
-			console.log(x, y);
-
-			_chart.point(_plot.xToChart(x), _plot.yToChart(y));
+			$("#bifurcationLegend").remove();
+			if ((x >= 0.0 && x <= 1.0) && (y >= 0.0 && y <= 4.0)) {
+				let g = _chart.group("bifurcationLegend");
+				_chart.text(g, 25, 10, `(${s.numberFormat(x, 3)}, ${s.numberFormat(y, 3)})`);
+			}
 		});
-
 	}
 
 }
